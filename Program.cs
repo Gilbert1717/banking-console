18e0491712a737034579c9ad929abe61e77d7a45
gilbert <s3665887@student.rmit.edu.au>
2023-01-06 21:36:21 +1100

    Seperate switch from menu display method

--- Program.cs
+++ Program.cs
@@ -1,45 +1,48 @@
 ﻿using System;
-using System.Data;
-using Microsoft.Data.SqlClient;
+// using System.Data;
+// using Microsoft.Data.SqlClient;
 using s3665887_a1;
 
 class Program
 {
     static void Main()
     {
-        Menu.displayMenu();
-        string menuSelect = Console.ReadLine();
-        if (menuSelect == "5")
+        string? menuSelect = null;
+        do
         {
-            Console.WriteLine("Successfully Logout");
-        }
-        
-        else if (menuSelect == "6")
-        {
-            Console.WriteLine("Thanks for using the system6");
-        }
-        
-        while (menuSelect != "5"&&menuSelect != "6")
-        {
-            switch (menuSelect)
-            {
-                case "1":
-                    Menu.deposit();
-                    break;
-                case "2":
-                    Menu.withdraw();
-                    break;
-                case "3":
-                    Menu.transfer();
-                    break;
-                case "4":
-                    Menu.myStatement();
-                    break;
-                default:
-                    break;
-            }
             Menu.displayMenu();
             menuSelect = Console.ReadLine();
+            menuSwitch(menuSelect);
+        } while (menuSelect != "5" && menuSelect != "6");
+    }
+
+    private static void menuSwitch(string s)
+    {
+        switch (s)
+        {
+            case "1":
+                Menu.deposit();
+                break;
+            case "2":
+                Menu.withdraw();
+                break;
+            case "3":
+                Menu.transfer();
+                break;
+            case "4":
+                Menu.myStatement();
+                break;
+            case "5":
+                Console.WriteLine("Successfully Logout");
+                break;
+            case "6":
+                Console.WriteLine("Thanks for using the system6");
+                break;
+            default:
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nInvalid input\n");
+                Console.ResetColor();
+                break;
         }
     }
 }
